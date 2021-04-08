ad8ef9139a92c708fe50751d33d683e1973198f5
Yorcal <43748206+Yorcal@users.noreply.github.com>
2021-04-08 12:02:33 +0200

    Add files via upload
    Logo qui tourne

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import logo from './images/Logo_Volt.png';
 import './App.css';
 import CustomNavbar from "./component/navbar.js"
@@ -13,19 +12,3 @@ function App() {
 }
 
 export default App;
-=======
-// import logo from './images/Logo_Volt.png';
-import './App.css';
-import CustomNavbar from "./component/navbar.js"
-import CustomVerticalNav from "./component/verticalNav.js"
-
-
-function App() {
-  return (<>
-    <CustomNavbar />
-    <CustomVerticalNav />
-  </>);
-}
-
-export default App;
->>>>>>> 13d39af02c2a14362c29455cecb344db75bb0480
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -6,12 +5,13 @@ import Nav from 'react-bootstrap/Nav'
 import logo from '../images/Logo_Volt.png';
 import textLogo from '../images/unknown.png';
 import "../styles/navbar.css"
+import "../App.css"
 
 
 function CustomNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" className="colorNavbar" variant="dark">
-            <Navbar.Brand href="#home"><img className="logo" src={logo} alt='VoltLogo'></img><img className="textLogo" src={textLogo} alt='VoltText'></img></Navbar.Brand>
+            <Navbar.Brand href="#home"><img className="App-logo logo" src={logo} alt='VoltLogo'></img><img className="textLogo" src={textLogo} alt='VoltText'></img></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto" >
@@ -29,35 +29,3 @@ function CustomNavbar() {
   
   export default CustomNavbar;
 
-=======
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-// import Container from 'react-bootstrap/Container'
-import logo from '../images/Logo_Volt.png';
-import textLogo from '../images/unknown.png';
-import "../styles/navbar.css"
-
-
-function CustomNavbar() {
-    return (
-        <Navbar collapseOnSelect expand="lg" className="colorNavbar" variant="dark">
-            <Navbar.Brand href="#home"><img className="logo" src={logo} alt='VoltLogo'></img><img className="textLogo" src={textLogo} alt='VoltText'></img></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto" >
-                    <Nav.Link href="#features" className="fontStyle">NEWS</Nav.Link>
-                    <Nav.Link href="#pricing" className="fontStyle">VOLT</Nav.Link>
-                    <Nav.Link href="#deets" className="fontStyle">TEAMS</Nav.Link>
-                    <Nav.Link href="#deets" className="fontStyle">WEBTV</Nav.Link>
-                    <Nav.Link href="#deets" className="fontStyle">SHOP</Nav.Link>
-                    <Nav.Link href="#deets" className="fontStyle">PRESS</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-  }
-  
-  export default CustomNavbar;
-
->>>>>>> 13d39af02c2a14362c29455cecb344db75bb0480
