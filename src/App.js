6648a326450c92f2a7db763e089c6d9005224cef
Yorcal <43748206+Yorcal@users.noreply.github.com>
2021-04-08 13:29:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // import logo from './images/Logo_Volt.png';
+import { Component } from 'react';
 import './App.css';
 import CustomNavbar from "./component/navbar.js"
 import CustomVerticalNav from "./component/verticalNav.js"
+import Container from 'react-bootstrap/Container'
 
 
 function App() {
   return (<>
     <CustomNavbar />
-    <CustomVerticalNav />
+    <Container maxWidth="sm">
+      <CustomVerticalNav />
+    </Container>
   </>);
 }
 
--- src/component/verticalNav.js
+++ src/component/verticalNav.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
+// import Container from 'react-bootstrap/Container'
 
 function VerticalBar() {
     return (
-        <Container>
-            <Navbar collapseOnSelect expand="lg" className="colorNavbar" variant="dark">
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav defaultActiveKey="/home" className="flex-column">
-                        <ul>
-                            <li><Nav.Link href="/home"></Nav.Link></li>
-                            <li><Nav.Link eventKey="link-1"></Nav.Link></li>
-                            <li><Nav.Link eventKey="link-2"></Nav.Link></li>
-                            <li><Nav.Link eventKey="link-3"></Nav.Link></li>
-                            <li><Nav.Link eventKey="link-4"></Nav.Link></li>
-                        </ul>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
+        <Navbar collapseOnSelect expand="lg" className="colorNavbar" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav defaultActiveKey="/home" className="flex-column">
+                    <ul>
+                        <li><Nav.Link href="/home"></Nav.Link></li>
+                        <li><Nav.Link eventKey="link-1"></Nav.Link></li>
+                        <li><Nav.Link eventKey="link-2"></Nav.Link></li>
+                        <li><Nav.Link eventKey="link-3"></Nav.Link></li>
+                        <li><Nav.Link eventKey="link-4"></Nav.Link></li>
+                    </ul>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
   }
   
